ea2d09026399796d4294c75c94a14f119884ae74
Alexandru Zbarcea <alexz@apache.org>
2023-06-07 11:32:12 -0400

    moving src to src/src

--- src/s/main/java/com/hackathon/util/CustomHibernateDaoSupport.java
+++ src/s/main/java/com/hackathon/util/CustomHibernateDaoSupport.java
@@ -4,7 +4,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public abstract class CustomHibernateDaoSupport extends HibssernateDaoSupport {
+public abstract class CustomHibernateDaoSupport extends HibernateDaoSupport {
 
 	@Autowired
 	public void anyMethodName(SessionFactory sessionFactory)
